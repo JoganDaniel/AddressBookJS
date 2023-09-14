bf4b10d5af16b017f644b08de98e1bbc5aca23c5
Jogan Daniel A <jogan.d@tvsnext.com>
2023-09-14 18:47:08 +0530

    [jogan] Add : Validations added

--- AddressBookJS/AddressBook.js
+++ AddressBookJS/AddressBook.js
@@ -21,6 +21,35 @@ class AddressBook {
             console.log();
         });
     }
+
+    validateContact(contact)
+    {
+        if (!/^[A-Z][a-zA-Z]{2,}$/.test(contact.firstName)) {
+            throw new Error(`First name should start with a capital letter and have at least 3 characters.`);
+        }
+        if (!/^[A-Z][a-zA-Z]{2,}$/.test(contact.lastName)) {
+            throw new Error(`Last name should start with a capital letter and have at least 3 characters.`);
+        }
+        if (contact.address.length < 4) {
+            throw new Error(`Address should have at least 4 characters.`);
+        }
+        if (contact.city.length < 4) {
+            throw new Error(`city should have at least 4 characters.`);
+        }
+        if (contact.state.length < 4) {
+            throw new Error(`state should have at least 4 characters.`);
+        }
+        if (! /^\d{6}$/.test(contact.zip)) {
+            throw new Error(`Invalid zipcode.`);
+        }
+        if (! /^\d{10}$/.test(contact.phoneNumber)) {
+            throw new Error(`Invalid Phone.`);
+        }
+        if (! /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(contact.email)) {
+            throw new Error(`Invalid email.`);
+        }
+        
+    }
 }
 
 module.exports = AddressBook;
--- AddressBookJS/Main.js
+++ AddressBookJS/Main.js
@@ -3,10 +3,12 @@ const AddressBook = require('./AddressBook');
 
 const addressBook = new AddressBook();
 
-const contact1 = new Contact("a", "a", "a", "a", "a", "1234", "99999999", "a@a.com");
-const contact2 = new Contact("b", "b", "b", "b", "B", "98765", "777777", "b@b.com");
+const contact1 = new Contact("Aaa", "Aba", "wedfa", "awefewf", "wefea", "123994", "9999999977", "a@a.com");
+const contact2 = new Contact("Baa", "Ffrgra", "wedfa", "awefewf", "tegtgtrg", "123994", "9995599977", "b@b.com");
 
+addressBook.validateContact(contact1);
 addressBook.addContact(contact1);
-addressBook.addContact(contact2);
+addressBook.validateContact(contact2);
 
+addressBook.addContact(contact2);
 addressBook.displayContacts();
