7d1fb080f39dee9a8c9a9fd7387ddc5e4954c25c
Jogan Daniel A <jogan.d@tvsnext.com>
2023-09-15 12:43:40 +0530

    [jogan] Add : Contact added

--- AddressBookJS/Main.js
+++ AddressBookJS/Main.js
@@ -5,10 +5,12 @@ const addressBook = new AddressBook();
 
 const contact1 = new Contact("Aaa", "Aba", "wedfa", "awefewf", "wefea", "123994", "9999999977", "a@a.com");
 const contact2 = new Contact("Baa", "Ffrgra", "wedfa", "awefewf", "tegtgtrg", "123994", "9995599977", "b@b.com");
+const contact3 = new Contact("Casj", "Trtgrg", "gtgrgw", "trghrt", "ljjgfe", "877866", "9993459977", "c@c.com");
 
 addressBook.validateContact(contact1);
 addressBook.addContact(contact1);
 addressBook.validateContact(contact2);
-
 addressBook.addContact(contact2);
+addressBook.validateContact(contact3);
+addressBook.addContact(contact3);
 addressBook.displayContacts();
